8438007762771670e931019b1ee3d043f0ba621c
sharakashQA <akashshar32@gmail.com>
2026-03-20 00:18:44 +0530

    Logging feature

--- src/test/java/stepDefinations/StepDefinations.java
+++ src/test/java/stepDefinations/StepDefinations.java
@@ -1,6 +1,7 @@
 package stepDefinations;
 
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,30 +31,31 @@ public class StepDefinations extends Utils {
 	 
 	 TestDataBuild data = new TestDataBuild();
 	@Given("Add Place Payload")
-	public void add_place_payload() {
+	public void add_place_payload() throws FileNotFoundException {
 		
-		 resspec = new ResponseSpecBuilder().expectStatusCode(200)
-				.expectContentType(ContentType.JSON).build();
-		
-		 res=given().spec(requestSpecification()).body(data.addplacePayload());
+		 res=given().spec(requestSpecification())
+				 .body(data.addplacePayload());
 		
 	}
 	
 	@When("User calls {string} with POST http request")
 	public void user_calls_with_post_http_request(String string) {
+		resspec = new ResponseSpecBuilder().expectStatusCode(200)
+				.expectContentType(ContentType.JSON).build();
 	    response = res.when().post("/maps/api/place/add/json").
-	    						then().spec(resspec).extract().response();
+	    			then().spec(resspec).extract().response();
 	}
 	
 	@Then("API call got success with status code {int}")
 	public void api_call_got_success_with_status_code(Integer int1) {
 	assertEquals(response.getStatusCode(), 200);
-	 
 	}
 	@Then("{string} in response body is {string}")
 	public void in_response_body_is(String keyValue, String ExpectedValue) {
 		String resp = response.asString();
 		JsonPath js = new JsonPath(resp);
 		assertEquals(js.get(keyValue).toString(), ExpectedValue);
+		
+		
 	    }
 	}
