85687557e787ea6502d0b8c005b89b3342fd4c54
sharakashQA <akashshar32@gmail.com>
2026-03-25 08:15:08 +0530

    using existing placeId for Get request

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -11,6 +11,8 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Utils {
@@ -39,4 +41,9 @@ public class Utils {
 		return prop.getProperty(key);
 	}
 
+	public String getJsonPath(Response response, String key) {
+		String resp = response.asString();
+		JsonPath js = new JsonPath(resp);
+		return js.get(key).toString();
+	}
 }
--- src/test/java/stepDefinations/StepDefinations.java
+++ src/test/java/stepDefinations/StepDefinations.java
@@ -29,7 +29,7 @@ public class StepDefinations extends Utils {
 	RequestSpecification res;
 	ResponseSpecification resspec;
 	Response response;
-	 
+	
 	 TestDataBuild data = new TestDataBuild();
 	 @Given("Add Place Payload with {string} {string} {string}")
 	 public void add_place_payload_with(String name, String language, String address) throws IOException {
@@ -64,11 +64,19 @@ public class StepDefinations extends Utils {
 	}
 	@Then("{string} in response body is {string}")
 	public void in_response_body_is(String keyValue, String ExpectedValue) {
-		String resp = response.asString();
-		JsonPath js = new JsonPath(resp);
-		System.out.println(js.get(keyValue).toString());
-		assertEquals(js.get(keyValue).toString(), ExpectedValue);
+		
+		assertEquals(getJsonPath(response, keyValue), ExpectedValue);
 		
 		
 	    }
+	@Then("verify place_id created maps to {string} using {string}")
+	public void verify_place_id_created_maps_to_using(String expectedName, String resource) throws IOException {
+		//get API call
+	    String place_id=getJsonPath(response, "place_id");
+		res=given().spec(requestSpecification()).queryParam("place_id", place_id);
+		user_calls_with_http_request(resource, "GET");
+		String actualName=getJsonPath(response, "name");
+		assertEquals(actualName, expectedName);
+	}
+	
 	}
